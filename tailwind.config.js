96a30ef36348e853e61519d8286aa857204d0476
Dimas Dharma Setiawan <dimasdharmasetiawan25@gmail.com>
2026-01-04 19:56:19 +0700

    fisrt commit upload projek laravel react

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            keyframes: {
+                floatSlow: {
+                    "0%": { transform: "translateY(0px)" },
+                    "50%": { transform: "translateY(40px)" },
+                    "100%": { transform: "translateY(0px)" },
+                },
+                floatSlowReverse: {
+                    "0%": { transform: "translateY(0px)" },
+                    "50%": { transform: "translateY(-40px)" },
+                    "100%": { transform: "translateY(0px)" },
+                },
+            },
+            animation: {
+                floatSlow: "floatSlow 12s ease-in-out infinite",
+                floatSlowReverse: "floatSlowReverse 25s ease-in-out infinite",
+            },
+        },
+    },
+
+    plugins: [forms],
+};
